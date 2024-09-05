ed8790d7864822e692c73ef07f5f98be99e547a9
Md Abdul Bari <91346898+bari199@users.noreply.github.com>
2024-09-05 21:49:42 +0530

    Adding Email Subscription App
    Email Subscription App using html, css ,js

---
+++ Email Subscription/app.js
@@ -0,0 +1,12 @@
+  const scriptURL = ''
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Thank You For Subscribing!"
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
