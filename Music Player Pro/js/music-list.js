b6ca9285d2056f8ca3919c97aa0ab568e4975619
Md Abdul Bari <91346898+bari199@users.noreply.github.com>
2024-09-20 05:01:44 -0700

    Update music-list.js
    changing the artist names , before the name added backtrick (~) .

--- Music Player Pro/js/music-list.js
+++ Music Player Pro/js/music-list.js
@@ -1,49 +1,49 @@
 let allMusic = [
     {
         name: "Hymn for the Weekend",
-        artist:"Coldplay",
+        artist:"~Coldplay~",
         img:"music-1",
         src:"music-1"
     },
     {
         name: "Something Just Like This",
-        artist:"The Chainsmokers,Coldplay",
+        artist:"~The Chainsmokers,Coldplay~",
         img:"music-2",
         src:"music-2"
     },
     {
         name: "Alone",
-        artist:"Alan Walker",
+        artist:"~Alan Walker~",
         img:"music-3",
         src:"music-3"
     },
     {
         name: "Unstoppable",
-        artist:"Sia",
+        artist:"~Sia~",
         img:"music-4",
         src:"music-4"
     },
     {
         name: "Let Me Down Slowly",
-        artist:"Alec Benjamin",
+        artist:"~Alec Benjamin~",
         img:"music-5",
         src:"music-5"
     },
     {
         name: "Perfect",
-        artist:"Ed Sheeran",
+        artist:"~Ed Sheeran~",
         img:"music-6",
         src:"music-6"
     },
     {
         name: "Thinking Out Loud",
-        artist:"Ed Sheeran",
+        artist:"~Ed Sheeran~",
         img:"music-7",
         src:"music-7"
     },
     {
         name: "A Thousand Year",
-        artist:"Christina Perri",
+        artist:"~Christina Perri~",
         img:"music-8",
         src:"music-8"
     }
